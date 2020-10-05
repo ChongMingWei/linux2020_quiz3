bf2b1cc015c2fb5f18901d68b9cb271015475825
ChongMingWei <29A7882zz@gmail.com>
2020-10-05 21:02:11 +0800

    Implement advanced questions of test3

--- test3.c
+++ test3.c
@@ -6,3 +6,47 @@ int numberOfSteps (int num)
     // https://leetcode.com/problems/number-of-steps-to-reduce-a-number-to-zero/
     return num ? __builtin_popcount(num) + 31 - __builtin_clz(num) : 0; //AAA BBB
 }
+int numberOfSteps_m (int num)
+{
+    /* Compute population count */
+    uint32_t popcount = num;
+    uint32_t tmp;
+    tmp = (popcount>>1)&0x77777777;
+    popcount -= tmp;
+    tmp = (tmp>>1)&0x77777777;
+    popcount -= tmp;
+    tmp = (tmp>>1)&0x77777777;
+    popcount -= tmp;
+
+    popcount += (popcount >> 4);
+    popcount &= 0x0f0f0f0f;
+    popcount *= 0x01010101;
+    popcount >>= 24;
+    /* Compute clz */
+    uint32_t n = 32, c = 16, l, r = num;
+    do {
+        l = r >> c;
+        if (l) { 
+            n -= c; 
+            r = l; 
+        }
+        c >>= 1;
+    } while (c);
+    
+    return num ? popcount + 31 - (n - r) : 0; //AAA BBB
+}
+
+int numberOfSteps_branchless (int num)
+{
+    int numofminus=0;//要做-1的次數
+    int numofdiv=0;
+    while(num){
+        int isodd = num&0x1;//確認目前 bit0 是否為1
+        int iseven = (num&0x1)^0x1;//確認目前 bit0 是否為0
+        numofminus += isodd;
+        numofdiv += iseven;
+        num -= (isodd-iseven)>0;//是奇數做-1
+        num >>= (iseven-isodd)>0;//是偶數做/2
+    }
+    return numofminus+numofdiv;
+}
--- test3.h
+++ test3.h
@@ -1,4 +1,6 @@
 #ifndef TEST3_H
 #define TEST3_H
 int numberOfSteps (int num);
+int numberOfSteps_m (int num);
+int numberOfSteps_branchless (int num);
 #endif

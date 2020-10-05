40676402eb3a5ab89687decc8a4a61bb8e9e887d
ChongMingWei <29A7882zz@gmail.com>
2020-10-05 21:01:31 +0800

    Implement advanced question of test2

--- test2.c
+++ test2.c
@@ -4,3 +4,13 @@ bool isPowerOfFour(int num)
 {
     return num > 0 && (num & (num - 1))==0 && !(__builtin_ctz(num) & 0x1); //OPQ
 }
+
+bool isPowerOfFour_m(int num)
+{
+    return num > 0 && __builtin_popcount(num)==1 && (num&0x55555555);
+}
+
+int bitwiseComplement(int N)
+{  
+    return N==0?1:~N&~((0xfffffffe) << (31-__builtin_clz(N))); 
+}
--- test2.h
+++ test2.h
@@ -2,4 +2,6 @@
 #define TEST2_H
 #include <stdbool.h>
 bool isPowerOfFour(int num);
+bool isPowerOfFour_m(int num);
+int bitwiseComplement(int N);
 #endif
